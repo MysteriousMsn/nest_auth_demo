12c98424597fb0a6cb3ca95332162eb0aa1d531c
Mohsin <mohsin.sunasara@manektech.com>
2024-03-04 13:02:34 +0530

    implemented nest auth with passport local, session and cookie

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { UsersModule } from 'src/users/users.module';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.strategy';
+import { SessionSerializer } from './session_setializer';
+
+@Module({
+  imports:[UsersModule, PassportModule, PassportModule.register({session: true})],
+  providers: [AuthService, LocalStrategy, SessionSerializer]
+})
+export class AuthModule {}
